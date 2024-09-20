6e46f8f9aa6360e21fa57f5760503ef8984799d1
M Redin Birezqic <muhammadredin.b@gmail.com>
2024-09-21 03:15:39 +0700

    initial commit

---
+++ src/main/java/io/github/muhammadredin/securenotesbackend/SecureNotesBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.muhammadredin.securenotesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureNotesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureNotesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/muhammadredin/securenotesbackend/SecureNotesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.muhammadredin.securenotesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureNotesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
